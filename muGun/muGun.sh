b3bb18b72ac33ffed8aaf0b6eab8bfe5ede471c1
Ivaylo Dionisov <ivaylo.dionisov@cern.ch>
2025-01-17 11:24:37 +0200

    Minor changes

--- muGun/muGun.sh
+++ muGun/muGun.sh
@@ -10,12 +10,15 @@ E_low="$3"
 E_high="$4"
 x_start_cm="${5:--21}"
 y_start_cm="${6:-31}"
-z_start_cm="${7:--300}"
+z_start_cm="${7:--1000}"
 PID="${8:-13}"
 
 ending_event=$(( starting_event+num_of_events ))
 
-output_dir=${HOME}/0_Workdir/muon_flux_PbPb.2023/particle_gun_sim/data/pGun_muons_${E_low}-${E_high}GeV_${starting_event}-${ending_event}
+output_dir=/EOS/user/i/idioniso/1_Data/Monte_Carlo/muGun/sim/pGun_muons_${E_low}-${E_high}GeV_${starting_event}-${ending_event}
+output_reco=/EOS/user/i/idioniso/1_Data/Monte_Carlo/muGun/reco
+mkdir -p $output_dir
+mkdir -p $output_reco
 digi_input=${output_dir}/sndLHC.PG_${PID}-TGeant4.root
 digi_geofile=${output_dir}/geofile_full.PG_${PID}-TGeant4.root
 
@@ -40,7 +43,12 @@ python ${SND_HOME}/sndsw/shipLHC/run_digiSND.py \
 print_elapsed_time $start_time
 printf "\n"
 
-python ${SND_HOME}/sndsw/shipLHC/scripts/run_TrackSelections.py -f ./sndLHC.PG_${PID}-TGeant4_digCPP.root  -g ./geofile_full.PG_${PID}-TGeant4.root  -o ../muon_reco_MC.gun_${E_low}-${E_high}GeV_${starting_event}-${ending_event}.root  -n ${num_of_events}  -s 0  -sc 1  -ht -st
+python ${SND_HOME}/sndsw/shipLHC/scripts/run_TrackSelections.py \
+    -f ${output_dir}/sndLHC.PG_${PID}-TGeant4_digCPP.root  \
+    -g ${output_dir}/geofile_full.PG_${PID}-TGeant4.root  \
+    -o ${output_reco}/muon_reco_MC.gun_${E_low}-${E_high}GeV_${starting_event}-${ending_event}.root  \
+    -n ${num_of_events}  \
+    -s 0  -sc 1  -ht -st
 
 
 print_elapsed_time -n $start_time
